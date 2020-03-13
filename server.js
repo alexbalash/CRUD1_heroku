5ceab8986da93cae987efa0c7a5d251bd8dcf6b0
alexbalash <55133359+alexbalash@users.noreply.github.com>
2020-03-13 20:27:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ app.use(express.json({ extended:false}))
 app.get('/', (req, res)=> res.send('API running'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
